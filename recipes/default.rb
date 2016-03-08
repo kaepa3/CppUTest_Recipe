7b62aed539e0b83f9287c0e0fb7af697a2a43ec7
山崎孝洋 <tak_3_zaki3@yahoo.co.jp>
2016-03-08 10:20:07 +0900

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,47 @@
+#
+# Cookbook Name:: CppUTest
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+bash "apt-get-update-periodic" do
+  code "apt-get update"
+  ignore_failure true
+end
+
+%w(g++ ).each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+#CppUTestのコンパイルに必要なモジュールのインストール
+%w(automake autoconf libtool git).each do |package|
+  package  "#{package}" do
+    action :install
+  end
+end
+
+
+
+#CppUTestのリポジトリをcloneする
+git "/home/.CppUTest" do
+  repository "git://github.com/cpputest/cpputest.git"
+  reference "master"
+  action :checkout
+  enable_checkout false
+end
+#Makefileの自動生成、コンパイル、lib、includeの配備
+bash "Makefile Generate" do
+  not_if { File.exists?("/usr/local/lib/libCppUTest.a") }
+  cwd "/home/.CppUTest/cpputest_build"
+  code <<-EOS
+    autoreconf .. -i
+    ../configure
+    make install
+  EOS
+  action :run
+end
